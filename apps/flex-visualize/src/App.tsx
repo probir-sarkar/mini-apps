4eb5958dad3ee84cee133d7df606c5fc95b80c04
Probir Sarkar <probirsarkar855@gmail.com>
2025-07-09 21:21:26 +0530

    color update visualizer

--- apps/flex-visualize/src/App.tsx
+++ apps/flex-visualize/src/App.tsx
@@ -1,16 +1,20 @@
 import FlexSection from "./components/flex-section";
+import { ModeToggle } from "./components/mode-toggle";
 import PropertySelector from "./components/property-selector";
 
 function App() {
   return (
-    <main className="max-h-screen overflow-hidden bg-neutral-900 flex flex-col text-primary">
-      <nav className="flex justify-center items-center text-3xl text-primary font-semibold bg-secondary py-6">
-        Flexbox Visualizer
+    <main className="max-h-screen overflow-hidden bg-background flex flex-col text-primary">
+      <nav className="text-3xl text-primary font-semibold bg-secondary py-6">
+        <div className="flex justify-between items-center  w-11/12 mx-auto">
+          <div className=""></div>
+          Flexbox Visualizer
+          <ModeToggle />
+        </div>
       </nav>
       <div className="p-6 flex flex-1 overflow-hidden gap-6">
         <aside className="max-w-1/5 w-full bg-secondary flex-1 rounded-2xl overflow-hidden flex flex-col">
-          <div className="p-4   text-primary font-semibold text-xl bg-secondary shadow-2xs border-b border-primary">
-            {" "}
+          <div className="p-4  text-primary font-semibold text-xl bg-secondary shadow-2xs border-b border-primary">
             Flex Properties
           </div>
           <div className="flex-1 overflow-y-scroll my-2">
--- apps/flex-visualize/src/main.tsx
+++ apps/flex-visualize/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { FlexProvider } from './context-provider.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { FlexProvider } from "./context-provider.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FlexProvider>
-    <App />
-    </FlexProvider>
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <FlexProvider>
+        <App />
+      </FlexProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
